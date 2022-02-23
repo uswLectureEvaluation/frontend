436b9584ea199458c4084f19fd8c78c3e1a1b705
abc9455 <83450991+dldudwls888@users.noreply.github.com>
2022-02-23 17:31:25 +0900

    create: myinfodetail

---
+++ 이영진/pages/myinfodetail/testinformation.element.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+
+export const BoxString1 = styled.div`
+    font-size: 16px;
+    padding-top: 10px;
+    
+`
+
+export const BoxString2 = styled.span`
+    font-weight: bold;
+    font-size: 32px;
+`
+
+export const BoxString4 = styled.span`
+    font-size: 16px;    
+`
+
+
+export const BoxButton1 = styled.span`
+    margin: 5px;
+    padding: 5px 15px 5px 15px;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    border-radius: 10px;
+    background-color: rgb(239, 239, 239);
+    color: rgb(52, 152, 219);
+`
+export const BoxButton2 = styled.span`
+    margin: 5px;
+    padding: 5px 15px 5px 15px;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    border-radius: 10px;
+    background-color: rgb(239, 239, 239);
+    color: rgb(230, 126, 34);
+`
