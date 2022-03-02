d3d472886d1a25e6cab7f9173aa08a192f8c23ff
tooooo1 <wer4272@nate.com>
2022-03-02 18:00:31 +0900

    update : search 레이아웃

--- 정충일/il/src/pages/Search/index.jsx
+++ 정충일/il/src/pages/Search/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Styled from './styled';
 import { Positioner } from '../../components/Wrapper/styled'
+import MyEvaluation from '../../components/MyEvaluation'
 
 
 const Search = () => {
@@ -8,7 +9,14 @@ const Search = () => {
         <Positioner>
             <Styled.GlobalStyle />
             <Styled.Wrapper>
-
+                <div>
+                    <Styled.Title>검색 결과</Styled.Title>
+                    <Styled.Input />
+                </div>
+                <div>
+                    정렬 만족도 꿀강 배움 날짜 종합
+                </div>
+                <MyEvaluation />
             </Styled.Wrapper>
         </Positioner>
     )
--- 정충일/il/src/pages/Search/styled.js
+++ 정충일/il/src/pages/Search/styled.js
@@ -12,3 +12,17 @@ export const GlobalStyle = createGlobalStyle`
 
 export const Wrapper = styled.div`
 `
+
+export const Title = styled.div`
+    font-size : 2rem;
+    font-weight : 1000;
+    margin-bottom : 1rem;
+`;
+
+export const Input = styled.input`
+    width: 80%;
+    height: 60px;
+    background-color: #f2f2f2;
+    border: 1px solid #d2d2d2;
+    border-radius: 10px;
+`;
