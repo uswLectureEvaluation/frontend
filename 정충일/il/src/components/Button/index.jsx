e684b72ad74b15f49a9eb8229a581b2622ca9159
tooooo1 <wer4272@nate.com>
2022-03-11 14:09:09 +0900

    feat : id 식별자

--- 정충일/il/src/components/Button/index.jsx
+++ 정충일/il/src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as Styled from './styled';
 
-const Button = ({ children, onClick, color}) => (
-    <Styled.Wrapper onClick={onClick} background={color}>
+const Button = ({ children, onClick, color, id }) => (
+    <Styled.Wrapper onClick={onClick} background={color} id={id}>
             {children}
     </Styled.Wrapper>
 );
