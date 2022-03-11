d161a91b35a6cbfa449e7674ea3cc2fe91b40e8e
tooooo1 <wer4272@nate.com>
2022-03-11 15:01:11 +0900

    refactor : 로직 및 css 변경

--- 정충일/il/src/components/MyEvaluation/index.jsx
+++ 정충일/il/src/components/MyEvaluation/index.jsx
@@ -1,19 +1,45 @@
 import React, { useState } from 'react'
-import { BoxString1, BoxString2, BoxString3, BoxString4, BoxString5, BoxString6, BoxButton1, BoxButton2, ModalString1 } from './styled'
-import { Typography, Dialog, CssBaseline, Grid, Box, Container, createTheme, ThemeProvider } from "@material-ui/core";
-import Editevaluation from '../EditEvaluation'
-import { Option } from '../../pages/LectureInfo/styled'
+import { BoxString1, BoxString2, BoxString3, BoxString4, BoxString5, ModalString1, ModalString2 } from './styled'
+import {CssBaseline, Grid, Container } from "@material-ui/core";
+import Modal from 'react-modal';
+import * as Styled from './styled';
 
+const 모달스타일 = {
+	overlay: {
+		position: "fixed",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: "rgba(0, 0, 0, 0.4)",
+		zIndex: 1100,
+	},
+	content: {
+    display: "flex",
+		justifyContent: "center",
+		background: "#ffffff",
+		overflow: "auto",
+    maxWidth:"600px",
+    minWidth:"500px",
+    left: "50%",
+    top:"0%",
+    transform: "translate(-50%, 3%)",
+		WebkitOverflowScrolling: "touch",
+		borderRadius: "14px",
+		outline: "none",
+		zIndex: 1100,
+	},
+};
 
-const MyEvaluation = () => {
+const Myevaluation = () => {
 
-  let [subjectName, setSubjectName] = useState(['학문과 사고', '네트워크 개론'])
+  let [subjectName, setSubjectName] = useState(['학문과 사고', '네트워크 개론', '졸업프로젝트'])
 
   return (
       <Container component="main" maxWidth="md">
         <CssBaseline />
         {subjectName.map((name, index) =>
-          <Subject subjectName={subjectName} index={index} />,
+          <Subject subjectName={subjectName} setSubjectName={setSubjectName} index={index} />,
         )}
       </Container>
   );
@@ -21,19 +47,19 @@ const MyEvaluation = () => {
 
 export const Modal1 = () => {
   return (
-    <div>
+    <div style={{paddingBottom:'10px', paddingTop:'5px'}}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={4}>
           <ModalString1>만족도 ⭐⭐⭐⭐</ModalString1>
-          <ModalString1>조모임 ⭐⭐⭐⭐</ModalString1>
+          <ModalString2>조모임</ModalString2><ModalString2 style={{color:'rgb(190, 190, 190)'}}>없음</ModalString2>
         </Grid>
         <Grid item xs={12} sm={4}>
           <ModalString1>꿀강 지수 ⭐⭐⭐⭐</ModalString1>
-          <ModalString1>과제 ⭐⭐⭐⭐</ModalString1>
+          <ModalString2>과제</ModalString2><ModalString2 style={{color:'rgb(231, 76, 60)'}}>많음</ModalString2>
         </Grid>
         <Grid item xs={12} sm={4}>
           <ModalString1>배움 지수 ⭐⭐⭐⭐</ModalString1>
-          <ModalString1>학점 ⭐⭐⭐⭐</ModalString1>
+          <ModalString2>학점</ModalString2><ModalString2 style={{color:'rgb(231, 76, 60)'}}>까다로움</ModalString2>
         </Grid>
       </Grid>
     </div>
@@ -41,39 +67,52 @@ export const Modal1 = () => {
 }
 
 export const Subject = (props) => {
-  let [modal, setModal] = useState(false);
-  let [edit, setEdit] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  // const Delete = () => {
+  //   if(window.confirm("강의평가를 삭제하시겠습니까?")===true){
+  //     let arrayCopy = [...props.subjectName];
+  //     arrayCopy.shift();
+  //     props.setSubjectName(arrayCopy)
+  //   }else{ return }
+  // }
 
   return (
-    <Box noValidate sx={{
-      mt: 3,
-    //   border: '2px solid rgb(158,158,158)',
-    //   border: '2px solid rgba(158,158,158,.5)',
-          borderTop: '1px solid black',
-      padding: '20px',
-    //   borderRadius: '20px'
+    <div style={{
+      margin: "10px 0",
+      borderBottom: '2px solid rgba(158,158,158,.5)',
+      padding: '15px',
     }}>
+     
       <div style={{ marginBottom: '15px' }}>
         <BoxString1>2020-1</BoxString1>
-        <BoxButton2 style={{ float: "right" }}>신고</BoxButton2>
-        {/* <BoxButton1 onClick={() => { setEdit(!edit) }} style={{ float: "right" }}>수정</BoxButton1> */}
+        {/* <BoxButton2 onClick={()=> {Delete()}} style={{ float: "right" }}>삭제</BoxButton2>
+        <BoxButton1 onClick={()=> setModalIsOpen(true)} style={{ float: "right" }}>수정</BoxButton1> */}
       </div>
-      <div>
         <BoxString2>{props.subjectName[props.index]}</BoxString2>
         <BoxString4>이다미 교수님</BoxString4>
-      </div>
+      <Styled.TempMargin>
       <BoxString3>평균 지수</BoxString3>
-      <BoxString3 style={{ paddingLeft: '10px' }}>⭐⭐⭐⭐⭐</BoxString3>
-      <span>5.0</span>
-      <BoxString5 onClick={() => { setModal(!modal) }}>{modal === true ? '간략하게 보기 >' : '자세히 보기 >'}</BoxString5>
+      <BoxString3 style={{ padding: '0 10px', letterSpacing: '-2px' }}>⭐⭐⭐⭐⭐</BoxString3>
+      <span style={{ color: "#2f2f2f"}}>5.0</span>
+        <BoxString5 onClick={() => { setModal(!modal) }}>{modal === true ? '간략하게 보기 >' : '자세히 보기 >'}</BoxString5>
+      </Styled.TempMargin>
       {modal === true ? <Modal1 /> : null}
-      <BoxString6>가나다라마바사아자차카타파하가나다라마바사아자차카타파하
+      {/* <BoxString6>가나다라마바사아자차카타파하가나다라마바사아자차카타파하
         가나다라마바사아자차카타파하
         가나다라마바사아자차카타파하
-      </BoxString6>
-      {edit === true ? <Editevaluation /> : null}
-    </Box>
+      </BoxString6> */}
+      <Modal 
+      isOpen={modalIsOpen}
+			style={모달스타일}
+			 // 오버레이나 esc를 누르면 핸들러 동작
+			ariaHideApp={false}
+      onRequestClose={() => setModalIsOpen(false)}
+    	>
+    	</Modal>
+      </div>
   )
 }
 
-export default MyEvaluation
+export default Myevaluation
