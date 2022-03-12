1582360c1bdc1847bf93cf2ecc81e76b60df68ab
tooooo1 <wer4272@nate.com>
2022-03-12 12:54:52 +0900

    refactor : 통합본 불필요 hook 제거

--- 통합/src/Pages/Search/index.jsx
+++ 통합/src/Pages/Search/index.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as Styled from './styled';
 import { Positioner } from '../../components/Wrapper/styled'
 import MyEvaluation from '../../components/MyEvaluation'
 
 
 const Search = () => {
-    let [detail, setDetail] = useState(['정렬', '만족도', '꿀강', '배움', '날짜', '종합']);
+    const detail = ['정렬', '만족도', '꿀강', '배움', '날짜', '종합'];
     return (
         <Positioner>
             <Styled.GlobalStyle />
             <Styled.Wrapper>
-                <div>
-                    <Styled.Title>검색 결과</Styled.Title>
-                    <Styled.Input />
-                </div>
+                <Styled.Title>검색 결과</Styled.Title>
+                <Styled.Input />
+
                 <Styled.SearchResultWrapper>
                     { detail.map((name, index)=>
                         <Styled.SearchResultMenu detail={detail} index={index}>
@@ -21,6 +20,7 @@ const Search = () => {
                         </Styled.SearchResultMenu>
                     )}
                 </Styled.SearchResultWrapper>
+                
                 <Styled.MyEvaluationWrapper>
                     <MyEvaluation />
                 </Styled.MyEvaluationWrapper>
