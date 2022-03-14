66938d0f11a76d4344f29ca9ae0b9dd611d6f26f
tooooo1 <wer4272@nate.com>
2022-03-15 02:16:05 +0900

    update : 구체화 작업

--- 정충일/il/src/components/TestInfo/styled.js
+++ 정충일/il/src/components/TestInfo/styled.js
@@ -1,1 +1,24 @@
 import styled from 'styled-components';
+
+export const Positioner = styled.div`
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-weight: bold;
+    font-size: 2rem;
+    text-align: center;
+`;
+
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 40vh;
+`;
+
+export const Color = styled.span`
+    color: #4b89dc;
+`;
+
+export const Content = styled.div`
+    margin: 2rem 0;
+`;
