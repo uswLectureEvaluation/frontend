ad8d2d3cf45000e3f77c1f24866fe5af9d2bdf40
abc9455 <83450991+dldudwls888@users.noreply.github.com>
2022-03-14 18:52:19 +0900

    update: 로고 삭제 / 버튼 통일
    전체적으로 모바일 우선의 디자인으로 개편

--- 통합/src/Pages/Login/pwsearch.element.js
+++ 통합/src/Pages/Login/pwsearch.element.js
@@ -3,32 +3,36 @@ import styled from "styled-components";
 
 
 export const WrapperBox = styled.div`
-    border: 1px solid rgb(158,158,158);
-    border: 1px solid rgba(158,158,158,.5);
     padding: 40px;
-    border-radius: 10px;
-    
+    @media only screen and (max-width: 500px) {
+        border: 1px solid rgb(158,158,158);
+        border: 1px solid rgba(158,158,158,.5);
+        border-radius: 15px;
+     }
 `
-export const Logo = styled.div`
+/*export const Logo = styled.div`
     color: #4B89DC;
     font-size: 50px;
     font-family: HanSans;
     font-weight: bold
-`;
+`;*/
 
 export const BoldText = styled.div`
     padding-bottom: 30px;
     border-bottom: 2px solid rgb(158,158,158);
     margin-bottom: 20px;
-    font-size: 20px; 
-    font-weight: bold
+    font-size : 4vh;
+    font-weight : 1000;
+    text-align: center;
+    font-family: "Pretendard-Black";
+
 `
 export const NormalText = styled.div`
     font-size: 15px;
     font-weight: normal
 `
 
-export const SearchButton = styled(Button)`
+/*export const SearchButton = styled(Button)`
     &&{
         margin-top: 10px;
         font-size: 14px;
@@ -39,7 +43,34 @@ export const SearchButton = styled(Button)`
         background-color: rgb(52, 152, 219);
         padding: 5px 100px 5px 100px
     }
+`;*/
+
+
+export const SearchButton = styled.button`
+    margin-top: 10px;
+    width: 100%;
+    padding: 0 1rem;
+    padding-top: 0.6rem;
+    border:none;
+    padding-bottom: 0.5rem;
+    background: #4b89dc;
+    color: white;
+    text-align: center;
+    font-size: 0.9rem;
+    font-weight: bold;
+    border-radius: 12px;
+    cursor: pointer;
+    user-select: none;
+    transition: .3s all;
+    &:hover, &:active {
+        background: #4b10f2;
+    }
+    &:disabled {
+        background-color: rgba(170,170,170);
+        cursor : auto;
+    }
 `;
+
 
 export const SmallText = styled.div`
     font-size: 12px;
