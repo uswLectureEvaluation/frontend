b7b148fa6a93a6bd2190315d37880bee5501a189
tooooo1 <wer4272@nate.com>
2022-03-29 00:23:50 +0900

    refactor : MyInfo Page
    RENEWAL : 진행률(65%)
    
    성형수술 완료

--- src/Pages/MyInfo/index.jsx
+++ src/Pages/MyInfo/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import * as Styled from './styled';
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { myInfoApi } from '../../api/Api';
 
 const MyInfo = () => {
-    const navigate = useNavigate();
-
+    // const navigate = useNavigate();
+    const option = ['피드백 전송', '문의하기', '이용약관', '개인정보 처리 방침',
+'오픈소스 라이선스', '비밀번호 변경', '회원 탈퇴']
     const [db, setData] = useState({
         data: []
     })
@@ -14,24 +15,70 @@ const MyInfo = () => {
         myInfoApi(setData)
         }, []
     )
-
+    console.log(db)
     return (
         <Styled.Container>
+            <Styled.InfoWrapper>
+                <Styled.InfoTitle>내 정보</Styled.InfoTitle>
+            </Styled.InfoWrapper>
+            <Styled.Wrapper id='top'>
+                <Styled.Content id='top'>
+                    <Styled.Title>내 계정</Styled.Title>
+
+                    
+                    <Styled.FlexContainer>
+                        <Styled.OptionTitle>로그인 아이디</Styled.OptionTitle>
+                        <Styled.FlexContainer>{db.loginId}</Styled.FlexContainer>
+                    </Styled.FlexContainer>
+                    <Styled.FlexContainer>
+                        <Styled.OptionTitle>학교 인증 메일</Styled.OptionTitle>
+                        <Styled.FlexContainer>없음</Styled.FlexContainer>
+                    </Styled.FlexContainer>
+                </Styled.Content>
+                <Styled.Button onClick={()=>alert('내가 쓴 글')} background='#3DD3C4' >
+                    내가 쓴 글
+                </Styled.Button>
+            </Styled.Wrapper>
+
             <Styled.Wrapper>
                 <Styled.Content>
+                    <Styled.Title>현재 보유 포인트</Styled.Title>
 
+                    
+                    <Styled.FlexContainer>
+                        <Styled.OptionTitle>작성한 강의평가</Styled.OptionTitle>
+                        <Styled.FlexContainer>{db.writtenLecture}개</Styled.FlexContainer>
+                    </Styled.FlexContainer>
+                    <Styled.FlexContainer>
+                        <Styled.OptionTitle>작성한 시험정보</Styled.OptionTitle>
+                        <Styled.FlexContainer>{db.writtenExam}개</Styled.FlexContainer>
+                    </Styled.FlexContainer>
+                    <Styled.FlexContainer>
+                        <Styled.OptionTitle>시험정보 열람 횟수</Styled.OptionTitle>
+                        <Styled.FlexContainer>{db.viewExam}개</Styled.FlexContainer>
+                    </Styled.FlexContainer>
                 </Styled.Content>
             </Styled.Wrapper>
 
             <Styled.Wrapper>
                 <Styled.Content>
+                    <Styled.Title>포인트 제도 안내</Styled.Title>
+                    <div style={{height:"20vh"}}></div>
 
                 </Styled.Content>
             </Styled.Wrapper>
 
+
             <Styled.Wrapper>
                 <Styled.Content>
-
+                    <Styled.Title>이용 안내</Styled.Title>
+                    {
+                        option.map((i)=> (
+                            <Styled.FlexContainer>
+                                <Styled.FlexContainer id='last'>{i}</Styled.FlexContainer>
+                            </Styled.FlexContainer>
+                        ))
+                    }
                 </Styled.Content>
             </Styled.Wrapper>
         </Styled.Container>
--- src/Pages/MyInfo/styled.js
+++ src/Pages/MyInfo/styled.js
@@ -12,6 +12,10 @@ export const GlobalStyle = createGlobalStyle`
 export const Wrapper = styled.div`
     width: 100%;
     margin: 0 auto;
+
+    &#top {
+      display: flex;
+    }
 `
 export const Container = styled.div`
     display: flex;
@@ -25,4 +29,63 @@ export const Content = styled.div`
     border: 1px solid rgb(224, 224, 224);
     padding: 1rem 2rem;
     margin: 10px 0;
+
+    &#top {
+      width: 100%;
+      margin-right: 2rem;
+    }
+`
+export const Title = styled.div`
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: 3rem;
+`
+
+export const OptionTitle = styled.div`
+    display: flex;
+    color: #222222;
+    width: 150px;
+`
+
+export const FlexContainer = styled.div`
+    display: flex;
+    width: 45%;
+    padding-bottom: 0.5rem;
+    color: #a3a3a3;
+    &#col {
+        flex-direction: column;
+    }
+    &#last {
+      color: #222222;
+    }
+`
+
+export const Button = styled.button`
+    margin: 10px 0;
+    width: 30%;
+    padding: 0 1rem;
+    border:none;
+    background: ${(props) => props.background};
+    color: white;
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+    border-radius: 12px;
+    cursor: pointer;
+    user-select: none;
+`;
+
+export const InfoWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    padding: 1rem 0;
+`
+
+export const InfoTitle = styled.div`
+    display: flex;
+    font-size: 1.5rem;
+    font-weight: bold;
+    padding-top: 4rem;
+    padding-bottom: 2rem;
 `
