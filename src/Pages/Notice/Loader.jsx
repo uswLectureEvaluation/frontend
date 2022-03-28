51c75357374c393b83e91ba500d215e412adf3c6
tooooo1 <wer4272@nate.com>
2022-03-28 16:30:25 +0900

    css : 무한스크롤 로딩 색 변경
    메인 컬러로 변경

--- src/Pages/Notice/Loader.jsx
+++ src/Pages/Notice/Loader.jsx
@@ -5,7 +5,7 @@ import ReactLoading from "react-loading";
 const Loader = () => {
   return (
     <Styled.LoaderWrap>
-      <ReactLoading type="spin" color="#A593E0" />
+      <ReactLoading type="spin" color='#3dd3c4' />
     </Styled.LoaderWrap>
   );
 };
