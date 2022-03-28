d72d463f3163f5970de7ccf5d5f1cb7d57ec5b84
denebe <whtjdfo01@gmail.com>
2022-03-28 18:21:03 +0900

    color변수수정

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -20,36 +20,30 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export const Color = (props) => {
-
-    if(props == 'main'){
-        return '#3dd3c4'
-    }else if(props =='border'){
-        return '#e0e0e0'
-    }else if(props == 'font'){
-        return '#222222'
-    }else if(props == 'sub-font'){
-        return '#515151'
-    }else if(props =='hover'){
-        return '#76d6bc'
-    }else if(props=='accent'){
-        return '#ff7675'
-    }
-
-}
-   
-
+  switch (props) {
+    case "main":
+      return "#3dd3c4";
+    case "border":
+      return "#e0e0e0";
+    case "font":
+      return "#222222";
+    case "sub-font":
+      return "#515151";
+    case "hover":
+      return "#76d6bc";
+    case "accent":
+      return "#ff7675";
+  }
+};
 
 export const Container = styled.div`
-z-index: 1;
-width: 100%;
+  z-index: 1;
+  width: 100%;
 
-@media screen and (max-width: 991px) {
+  @media screen and (max-width: 991px) {
     padding-right: 30px;
     padding-left: 30px;
-}
+  }
 `;
 
-
-
-
-export default GlobalStyle
+export default GlobalStyle;
