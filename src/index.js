1199fd02f823459a5b53b5c9f2005244fa026fab
tooooo1 <wer4272@nate.com>
2022-04-05 22:03:04 +0900

    update : prettier 적용

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import { store } from "./app/store"
-import { Provider } from "react-redux"
-import { PersistGate } from "redux-persist/integration/react"
-import { persistStore } from "redux-persist"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
-let persistor = persistStore(store)
+let persistor = persistStore(store);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <App />
-            </PersistGate>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-)
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
