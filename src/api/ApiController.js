3e35e17a9e43ee4985f0c5650a2017360621028c
aiden <rlaalsrb3559@naver.com>
2022-04-16 00:35:25 +0900

    fix: token 만료시 axios interceptor 로직 수정

--- src/api/ApiController.js
+++ src/api/ApiController.js
@@ -1,3 +1,4 @@
+import { ErrorOutline } from '@mui/icons-material';
 import axios from 'axios';
 import { Cookies } from 'react-cookie';
 
@@ -10,23 +11,13 @@ const instance = axios.create({
   timeout: 5000,
 });
 
-let isTokenRefreshing = false;
-let refreshSubscribers = [];
-
-const onTokenRefreshed = (AccessToken) => {
-  refreshSubscribers.map((callback) => callback(AccessToken));
-};
-
-const addRefreshSubscriber = (callback) => {
-  refreshSubscribers.push(callback);
-};
-
 instance.interceptors.request.use(
   function (config) {
-    //request 정상
-    config.headers['Content-Type'] = 'application/json';
-    config.headers['Authorization'] = cookies.get('AccessToken');
-    console.log(config);
+    const accessToken = cookies.get('AccessToken');
+    if (accessToken) {
+      config.headers['Content-Type'] = 'application/json';
+      config.headers['Authorization'] = accessToken;
+    }
 
     return config;
   },
@@ -41,49 +32,39 @@ instance.interceptors.response.use(
     return response.data;
   },
   async (error) => {
-    const {
-      config,
-      response: { status },
-    } = error;
-    const originalRequest = config;
-    if (status === 401) {
-      if (!isTokenRefreshing) {
-        // isTokenRefreshing이 false인 경우에만 token refresh 요청
-        isTokenRefreshing = true;
-        const RefreshToken = await cookies.get('RefreshToken');
-        const { data } = await axios({
-          url: `/user/refresh`, // 토큰 재요청
-          headers: {
-            Authorization: RefreshToken,
-          },
-          method: 'POST',
-        });
-        // 새로운 토큰 저장
-        const { AccessToken: newAccessToken, RefreshToken: newRefreshToken } = data;
-        await (cookies.set('AccessToken', newAccessToken, {
-          path: '/',
-          secure: true,
-          sameSite: false,
-        }),
-        cookies.set('RefreshToken', newRefreshToken, {
-          path: '/',
-          secure: true,
-          sameSite: false,
-        }));
-        isTokenRefreshing = false;
-        axios.defaults.headers.common['Authorization'] = newAccessToken;
-        // 새로운 토큰으로 지연되었던 요청 진행
-        onTokenRefreshed(newAccessToken);
-      }
-      // token이 재발급 되는 동안의 요청은 refreshSubscribers에 저장
-      const retryOriginalRequest = new Promise((resolve) => {
-        addRefreshSubscriber((AccessToken) => {
-          originalRequest.headers.Authorization = AccessToken;
-          resolve(instance(originalRequest));
-        });
+    const originalRequest = error.config;
+
+    if (error.response.status === 401) {
+      const RefreshToken = await cookies.get('RefreshToken');
+
+      const { data } = await axios({
+        url: `/user/refresh`, // 토큰 재요청
+        headers: {
+          Authorization: RefreshToken,
+        },
+        method: 'POST',
       });
+      console.log('새 토큰 반환 데이터ㅇ', data);
+      const { AccessToken: newAccessToken, RefreshToken: newRefreshToken } = data;
+      await cookies.set('AccessToken', newAccessToken, {
+        path: '/',
+        secure: true,
+        sameSite: false,
+      });
+      await cookies.set('RefreshToken', newRefreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: false,
+      });
+      originalRequest.headers['Authorization'] = newAccessToken;
+
+      const retryOriginalRequest = new Promise((resolve) => {
+        resolve(instance(originalRequest));
+      });
+
       return retryOriginalRequest;
     }
+
     return Promise.reject(error);
   }
 );
