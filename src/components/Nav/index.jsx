140fa4c27e57715b0c65c6e1142065ec31038e31
denebe <twhjdfo01@daum.net>
2022-05-01 18:14:44 +0900

    update:App버전 nav바 적용

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import * as Styled from './styled';
 import { Cookies } from 'react-cookie';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { VscChromeClose } from 'react-icons/vsc';
 // import { useSelector, useDispatch } from "react-redux"
 import { useNavigate } from 'react-router-dom';
 
@@ -13,7 +15,6 @@ const Nav = () => {
   const [click, setClick] = useState(false);
 
   const cookies = new Cookies();
-
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
@@ -28,7 +29,6 @@ const Nav = () => {
   };
 
   const logoutClick = () => {
-    
     cookies.remove('AccessToken');
     navigate('/login');
   };
@@ -38,17 +38,13 @@ const Nav = () => {
   return (
     <Styled.Navbar>
       {button ? (
-        <Styled.Img src="img/logo.png" width={110} onClick={() => navigate('/')} />
+        <Styled.NavLogo src="img/logo.png" width={110} onClick={() => navigate('/')} />
       ) : (
-        <Styled.Img src="img/tabletlogo.png" width={110} onClick={() => navigate('/')} />
+        <Styled.NavLogo src="img/tabletlogo.png" width={110} onClick={() => navigate('/')} />
       )}
-      {/* <Styled.MobileIcon onClick={handleClick}>
-        {click ? (
-          <Styled.Img src={"Styled.Img/icon_color_fire_36.svg"} />
-        ) : (
-          <Styled.Img src={"img/icon_color_bee_36.svg"} />
-        )}
-      </Styled.MobileIcon> */}
+      <Styled.MobileIcon onClick={handleClick}>
+        {click ? <VscChromeClose /> : <GiHamburgerMenu />}
+      </Styled.MobileIcon>
 
       <Styled.NavMenu onClick={handleClick} click={click}>
         <Styled.NavLinks onClick={() => navigate('notice')}>공지사항</Styled.NavLinks>
@@ -59,14 +55,13 @@ const Nav = () => {
         )}
         {cookies.get('AccessToken') == null ? (
           <Styled.NavLinks id="signup" onClick={() => navigate('signup')}>
-          회원가입
-        </Styled.NavLinks>
+            회원가입
+          </Styled.NavLinks>
         ) : (
           <Styled.NavLinks id="signup" onClick={() => navigate('myinformation')}>
-          내 정보
-        </Styled.NavLinks>
+            내 정보
+          </Styled.NavLinks>
         )}
-        
       </Styled.NavMenu>
     </Styled.Navbar>
   );
--- src/components/Nav/styled.js
+++ src/components/Nav/styled.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 
-
 export const Navbar = styled.nav`
   background: #ffffff;
   margin: 0 auto;
@@ -13,9 +12,19 @@ export const Navbar = styled.nav`
   z-index: 999;
   justify-content: space-around;
   border-bottom: 1.5px solid #e0e0e0;
+
+  @media screen and (max-width: 480px) {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    justify-content: space-between;
+  
+  }
 `;
 
-export const NavLogo = styled.nav`
+export const NavLogo = styled.img`
   color: #346cfd;
   display: flex;
   font-size: 2rem;
@@ -24,38 +33,44 @@ export const NavLogo = styled.nav`
     cursor: pointer;
   }
 
-  // @media screen and (max-width: 960px) {
-  //   margin: 0 auto;
-  // }
+  @media screen and (max-width: 480px) {
+    font-size: var(--font-medium);
+    font-weight: var(--weight-semi-bold);
+   
+  }
 `;
 
 export const MobileIcon = styled.div`
+
+  font-size: 2rem;
+  align-items: center;
+  color: #346cfd;
   display: none;
 
-  // @media screen and (max-width: 960px) {
-  //   display: block;
-  //   font-size: 1.8rem;
-  //   cursor: pointer;
-  //   position: absolute;
-  //   left: 0;
-  // }
+
+  @media screen and (max-width: 480px) {
+    
+    display: block;
+  }
 `;
 
 export const NavMenu = styled.nav`
   display: flex;
   padding-top: 3vh;
 
-  // @media screen and (max-width: 960px) {
-  //   flex-direction: column;
-  //   width: 100%;
-  //   height: 90vh;
-  //   position: absolute;
-  //   top: 80px;
-  //   left: ${({ click }) => (click ? 0 : '-100%')};
-  //   opacity: 1;
-  //   transition: all 0.5s ease;
-  //   background: #fff;
-  // }
+  @media screen and (max-width: 480px) {
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    left: ${({ click }) => (click ? 0 : '-100%')};
+    opacity: 1;
+    transition: all 0.5s ease;
+    background: #fff;
+    z-index: 999;
+    
+  }
 `;
 
 export const NavLinks = styled.div`
@@ -69,22 +84,19 @@ export const NavLinks = styled.div`
     color: #346cfd;
   }
 
-  // @media screen and (max-width: 960px) {
-  //   text-align: center;
-  //   padding: 2rem;
-  //   display: table;
-  //   &:hover {
-  //     color: #4b89dc;
-  //     transition: all 0.3s ease;
-  //   }
-  // }
+  @media screen and (max-width: 480px) {
+    text-align: center;
+    padding: 2rem;
+    display: table;
+    &:hover {
+      color: #4b89dc;
+      transition: all 0.3s ease;
+    }
+  }
 `;
 
 export const Img = styled.img`
-
   &:hover {
     cursor: pointer;
   }
-
-
 `;
