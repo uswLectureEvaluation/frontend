51b0bed840984f44ee5457d09047cedcd133734c
abc9455 <dldudwls888@gmail.com>
2022-05-02 02:53:16 +0900

    lectureInfo 강의평가 부분 디자인 업데이트

--- src/components/SearchEvaluationList/index.jsx
+++ src/components/SearchEvaluationList/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { BoxString5, YearText } from './styled';
-import { searchEvaluationApi } from '../../api/Api';
-import StarRatings from 'react-star-ratings';
 import * as Styled from './styled';
+import StarRatings from 'react-star-ratings';
+import { searchEvaluationApi } from '../../api/Api';
+
 
 export const DetailModal = (props) => {
   const teamSet = props.team;
@@ -18,76 +18,54 @@ export const DetailModal = (props) => {
     2: <Styled.DataColor id="purple">많음</Styled.DataColor>,
   };
   const difficulty = {
-    0: <Styled.DataColor>까다로움</Styled.DataColor>,
+    0: <Styled.DataColor id="purple">까다로움</Styled.DataColor>,
     1: <Styled.DataColor id="cyan">보통</Styled.DataColor>,
-    2: <Styled.DataColor id="purple">잘줌</Styled.DataColor>,
+    2: <Styled.DataColor>너그러움</Styled.DataColor>,
   };
 
   return (
     <div>
       <Styled.StarFlex id="top">
-        <Styled.StarFlex>
-          만족도
-          <Styled.PaddingRight />
-          <StarRatings
-            rating={props.satisfaction}
-            starRatedColor="#a3a3a3"
-            numberOfStars={5}
-            name="rating"
-            starDimension="18px"
-            starSpacing="0px"
-            svgIconPath="M17.563,21.56a1,1,0,0,1-.466-.115L12,18.765l-5.1,2.68a1,1,0,0,1-1.451-1.054l.974-5.676L2.3,10.7A1,1,0,0,1,2.856,8.99l5.7-.828L11.1,3A1.04,1.04,0,0,1,12.9,3l2.549,5.164,5.7.828A1,1,0,0,1,21.7,10.7l-4.124,4.02.974,5.676a1,1,0,0,1-.985,1.169Z"
-            svgIconViewBox="0 0 24 24"
-          />
-        </Styled.StarFlex>
-        <Styled.StarFlex>
-          꿀강 지수
-          <Styled.PaddingRight />
-          <StarRatings
-            rating={props.honey}
-            starRatedColor="#a3a3a3"
-            numberOfStars={5}
-            name="rating"
-            starDimension="18px"
-            starSpacing="0px"
-            svgIconPath="M17.563,21.56a1,1,0,0,1-.466-.115L12,18.765l-5.1,2.68a1,1,0,0,1-1.451-1.054l.974-5.676L2.3,10.7A1,1,0,0,1,2.856,8.99l5.7-.828L11.1,3A1.04,1.04,0,0,1,12.9,3l2.549,5.164,5.7.828A1,1,0,0,1,21.7,10.7l-4.124,4.02.974,5.676a1,1,0,0,1-.985,1.169Z"
-            svgIconViewBox="0 0 24 24"
-          />
-        </Styled.StarFlex>
-        <Styled.StarFlex>
-          배움 지수
-          <Styled.PaddingRight />
-          <StarRatings
-            rating={props.learning}
-            starRatedColor="#a3a3a3"
-            numberOfStars={5}
-            name="rating"
-            starDimension="18px"
-            starSpacing="0px"
-            svgIconPath="M17.563,21.56a1,1,0,0,1-.466-.115L12,18.765l-5.1,2.68a1,1,0,0,1-1.451-1.054l.974-5.676L2.3,10.7A1,1,0,0,1,2.856,8.99l5.7-.828L11.1,3A1.04,1.04,0,0,1,12.9,3l2.549,5.164,5.7.828A1,1,0,0,1,21.7,10.7l-4.124,4.02.974,5.676a1,1,0,0,1-.985,1.169Z"
-            svgIconViewBox="0 0 24 24"
-          />
-        </Styled.StarFlex>
-      </Styled.StarFlex>
-      <Styled.StarFlex id="bottom">
-        <Styled.StarFlex>조모임 {team[teamSet]}</Styled.StarFlex>
-        <Styled.StarFlex>과제 {homework[homeworkSet]}</Styled.StarFlex>
-        <Styled.StarFlex>학점 {difficulty[difficultySet]}</Styled.StarFlex>
+        <Styled.FlexContainer id="col">
+          <Styled.StarFlex id="between">
+            만족도
+            <Styled.PaddingRight />
+            <Styled.Rate id="modal">{props.satisfaction.toFixed(1)}</Styled.Rate>
+          </Styled.StarFlex>
+          <Styled.StarFlex id="between">조모임 {team[teamSet]}</Styled.StarFlex>
+        </Styled.FlexContainer>
+        <Styled.FlexContainer id="col">
+          <Styled.StarFlex id="between">
+            꿀강 지수
+            <Styled.PaddingRight />
+            <Styled.Rate id="modal">{props.honey.toFixed(1)}</Styled.Rate>
+          </Styled.StarFlex>
+          <Styled.StarFlex id="between">과제 {homework[homeworkSet]}</Styled.StarFlex>
+        </Styled.FlexContainer>
+        <Styled.FlexContainer id="col">
+          <Styled.StarFlex id="between">
+            배움 지수
+            <Styled.PaddingRight />
+            <Styled.Rate id="modal">{props.learning.toFixed(1)}</Styled.Rate>
+          </Styled.StarFlex>
+          <Styled.StarFlex id="between">학점 {difficulty[difficultySet]}</Styled.StarFlex>
+        </Styled.FlexContainer>
       </Styled.StarFlex>
     </div>
   );
 };
 
-const SearchEvaluationList = ({ selectId }) => {
+const SearchEvaluationList = ({selectId}) => {
   const [db, setData] = useState({
     data: [],
   });
   useEffect(() => {
     searchEvaluationApi(selectId).then((data) => setData(data));
+    console.log(db)
   }, [selectId]);
 
   return db.length !== 0 ? (
-    <Styled.FlexWrap>
+    <Styled.Wrapper>
       {db.data.map((v, i) => (
         <Subject
           key={v.id}
@@ -103,7 +81,7 @@ const SearchEvaluationList = ({ selectId }) => {
           id={v.id}
         />
       ))}
-    </Styled.FlexWrap>
+    </Styled.Wrapper>
   ) : (
     <div></div>
   );
@@ -112,52 +90,48 @@ const SearchEvaluationList = ({ selectId }) => {
 export const Subject = (props) => {
   const [modal, setModal] = useState(false);
   return (
-    <Styled.LectureWrapper>
-      <Styled.MarginTop id="top">
-        <div style={{ marginBottom: '10px' }}>
-          <YearText>{props.semester}</YearText>
-          <Styled.DeleteButton style={{ float: 'right' }}>신고</Styled.DeleteButton>
-        </div>
-        <Styled.TitleWrapper>
+    <div style={{ marginTop: '15px' }}>
+      <Styled.LectureWrapper>
+        <Styled.MarginTop id="top">
           <Styled.TitleWrapper>
-            <Styled.Title></Styled.Title>
-            <Styled.Professor></Styled.Professor>
+            <Styled.YearText>{props.semester}</Styled.YearText>
           </Styled.TitleWrapper>
-        </Styled.TitleWrapper>
-        <Styled.PaddingRight>평균지수</Styled.PaddingRight>
-        <StarRatings
-          rating={props.totalAvg}
-          starRatedColor="#346cfd"
-          numberOfStars={5}
-          name="rating"
-          starDimension="24px"
-          starSpacing="0px"
-          svgIconPath="M17.563,21.56a1,1,0,0,1-.466-.115L12,18.765l-5.1,2.68a1,1,0,0,1-1.451-1.054l.974-5.676L2.3,10.7A1,1,0,0,1,2.856,8.99l5.7-.828L11.1,3A1.04,1.04,0,0,1,12.9,3l2.549,5.164,5.7.828A1,1,0,0,1,21.7,10.7l-4.124,4.02.974,5.676a1,1,0,0,1-.985,1.169Z"
-          svgIconViewBox="0 0 24 24"
-        />
-        <Styled.Rate>{props.totalAvg.toFixed(1)}</Styled.Rate>
-        <BoxString5
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          {modal === true ? '간략히' : '자세히'}
-        </BoxString5>
-      </Styled.MarginTop>
-      {modal === true ? (
-        <DetailModal
-          satisfaction={props.satisfaction}
-          honey={props.honey}
-          learning={props.learning}
-          team={props.team}
-          homework={props.homework}
-          difficulty={props.difficulty}
-        />
-      ) : null}
-      <Styled.MarginTop id="bottom">
-        <Styled.EvaluationDetail>{props.content}</Styled.EvaluationDetail>
-      </Styled.MarginTop>
-    </Styled.LectureWrapper>
+          <Styled.EditButton>신고</Styled.EditButton>
+          <StarRatings
+            rating={props.totalAvg}
+            starRatedColor="#346cfd"
+            numberOfStars={5}
+            name="rating"
+            starDimension="18px"
+            starSpacing="0px"
+            svgIconPath="M17.563,21.56a1,1,0,0,1-.466-.115L12,18.765l-5.1,2.68a1,1,0,0,1-1.451-1.054l.974-5.676L2.3,10.7A1,1,0,0,1,2.856,8.99l5.7-.828L11.1,3A1.04,1.04,0,0,1,12.9,3l2.549,5.164,5.7.828A1,1,0,0,1,21.7,10.7l-4.124,4.02.974,5.676a1,1,0,0,1-.985,1.169Z"
+            svgIconViewBox="0 0 24 24"
+          />
+          <Styled.Rate>{props.totalAvg.toFixed(1)}</Styled.Rate>
+          <Styled.ModalOpen
+            onClick={() => {
+              setModal(!modal);
+            }}
+          >
+            {modal === true ? '간략히' : '자세히'}
+          </Styled.ModalOpen>
+        </Styled.MarginTop>
+        <div style={{ marginBottom: '5px' }} />
+        {modal === true ? (
+          <DetailModal
+            satisfaction={props.satisfaction}
+            honey={props.honey}
+            learning={props.learning}
+            team={props.team}
+            homework={props.homework}
+            difficulty={props.difficulty}
+          />
+        ) : null}
+        <Styled.MarginTop id="bottom">
+          <Styled.EvaluationDetail>{props.content}</Styled.EvaluationDetail>
+        </Styled.MarginTop>
+      </Styled.LectureWrapper>
+    </div>
   );
 };
 
