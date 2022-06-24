b003929253604e68084baa51927922eb7dd5bc8b
abc9455 <dldudwls888@gmail.com>
2022-06-24 17:15:50 +0900

    feat: 비밀번호 변경 기능 구현

--- src/Pages/ResetPassword/index.jsx
+++ src/Pages/ResetPassword/index.jsx
@@ -3,14 +3,17 @@ import { resetPasswordApi } from '../../api/Api'
 import * as Styled from './styled'
 
 const ResetPassword = () => {
-    const [password, setPassword] = useState()
+    const [newPassword, setNewPassword] = useState();
+    const [prePassword, setPrePassword] = useState();
     const handleChange = () => {
-        resetPasswordApi(password)
+        resetPasswordApi(prePassword, newPassword);
     }
+    
     return(
         <Styled.FlexContainer>
             <Styled.FlexBox>비밀번호 변경</Styled.FlexBox>
-            <input placeholder='새 비밀번호 입력' onChange={(e)=>{setPassword(e.target.value)}}/>
+            <input placeholder='기존 비밀번호 입력' onChange={(e)=>{setPrePassword(e.target.value)}}/>
+            <input placeholder='새 비밀번호 입력' onChange={(e)=>{setNewPassword(e.target.value)}}/>
             <button onClick={handleChange}>전송</button>
         </Styled.FlexContainer>
 
