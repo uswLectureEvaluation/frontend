56f53616831a871c61fc5b11b54cbc25f3ea6ede
denebe <twhjdfo01@daum.net>
2022-07-04 18:54:33 +0900

    [fix]비밀번호 변경 디자인

--- src/Pages/ResetPassword/index.jsx
+++ src/Pages/ResetPassword/index.jsx
@@ -1,23 +1,56 @@
-import {React, useState} from 'react'
-import { resetPasswordApi } from '../../api/Api'
-import * as Styled from './styled'
+import { React, useState } from 'react';
+import { resetPasswordApi } from '../../api/Api';
+import * as Styled from './styled';
 
 const ResetPassword = () => {
-    const [newPassword, setNewPassword] = useState();
-    const [prePassword, setPrePassword] = useState();
-    const handleChange = () => {
-        resetPasswordApi(prePassword, newPassword);
-    }
-    
-    return(
-        <Styled.FlexContainer>
-            <Styled.FlexBox>비밀번호 변경</Styled.FlexBox>
-            <input placeholder='기존 비밀번호 입력' onChange={(e)=>{setPrePassword(e.target.value)}}/>
-            <input placeholder='새 비밀번호 입력' onChange={(e)=>{setNewPassword(e.target.value)}}/>
-            <button onClick={handleChange}>전송</button>
-        </Styled.FlexContainer>
+  const [newPassword, setNewPassword] = useState();
+  const [prePassword, setPrePassword] = useState();
+  const handleChange = () => {
+    resetPasswordApi(prePassword, newPassword);
+  };
 
-    )
-}
+  return (
+    <Styled.Container>
+      <Styled.Img src="img/signup.svg" width={450} />
+      <Styled.LoginWrapper>
+        <Styled.Title>비밀번호 변경</Styled.Title>
+        <Styled.Sub>기존 비밀번호를 입력하세요</Styled.Sub>
+        <Styled.CssTextField
+          placeholder="기존 비밀번호 입력"
+          margin="normal"
+          required
+          id="outlined-basic"
+          autoFocus
+          onChange={(e) => {
+            setPrePassword(e.target.value);
+          }}
+        />
 
-export default ResetPassword
+        <Styled.Sub>새로운 비밀번호를 입력하세요</Styled.Sub>
+
+        <Styled.CssTextField
+          placeholder="새 비밀번호 입력"
+          margin="normal"
+          required
+          id="outlined-basic"
+          autoFocus
+          onChange={(e) => {
+            setNewPassword(e.target.value);
+          }}
+        />
+
+        <Styled.Button
+          background="#346cfd"
+          type="submit"
+          fullWidth
+          variant="contained"
+          onClick={handleChange}
+        >
+          전송
+        </Styled.Button>
+      </Styled.LoginWrapper>
+    </Styled.Container>
+  );
+};
+
+export default ResetPassword;
