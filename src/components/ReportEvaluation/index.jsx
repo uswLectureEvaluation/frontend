3749aedbe4540a6e65ce467eaa11bc5056b69318
tooooo1 <wer4272@nate.com>
2022-07-08 19:23:21 +0900

    feat: 신고 경고 문구 추가

--- src/components/ReportEvaluation/index.jsx
+++ src/components/ReportEvaluation/index.jsx
@@ -1,18 +1,27 @@
-import { useState } from "react"
-import { evaluateReportApi } from "../../api/Api"
+import { useState } from 'react';
+import { evaluateReportApi } from '../../api/Api';
 
 const ReportEvaluation = (props) => {
-    const [content, setContent] = useState();
-    const onReport = () => {
-        evaluateReportApi(props.evaluateIdx, content)
-    }
-    return(
-        <div>
-            <div>신고</div>
-            <textarea name="report" id="report" cols="30" rows="10" onChange={(e)=>{setContent(e.target.value)}}></textarea>
-            <button onClick={onReport}>전송</button>
-        </div>
-    )
-}
+  const [content, setContent] = useState();
+  const onReport = () => {
+    if (window.confirm('정말 신고하시겠어요? \n*허위 신고 시 제재가 가해질 수 있습니다!'))
+      evaluateReportApi(props.evaluateIdx, content);
+  };
+  return (
+    <div>
+      <div>신고</div>
+      <textarea
+        name="report"
+        id="report"
+        cols="30"
+        rows="10"
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      ></textarea>
+      <button onClick={onReport}>전송</button>
+    </div>
+  );
+};
 
-export default ReportEvaluation
+export default ReportEvaluation;
