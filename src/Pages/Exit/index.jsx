022ed68345712c5cf6fc45be2b7748922c0472a2
tooooo1 <wer4272@nate.com>
2022-07-08 19:09:03 +0900

    Feat: 회원탈퇴 문구 추가

--- src/Pages/Exit/index.jsx
+++ src/Pages/Exit/index.jsx
@@ -15,7 +15,7 @@ const Exit = () => {
   };
 
   const onSubmit = () => {
-    if (window.confirm('정말 탈퇴하시나요?')) {
+    if (window.confirm('회원탈퇴 시 작성한 강의평가/시험정보는 전부 삭제됩니다. \n정말 탈퇴하시나요?')) {
         quitApi(id, pw)
       } else {
         alert('취소')
