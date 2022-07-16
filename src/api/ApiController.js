d08bd04267a26f57a0137634a9e2ad969e7c3be4
abc9455 <dldudwls888@gmail.com>
2022-07-16 20:25:08 +0900

    fix: proxy 복구

--- src/api/ApiController.js
+++ src/api/ApiController.js
@@ -3,7 +3,7 @@ import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
-const PROXY_URL = window.location.hostname === 'localhost' ? '' : '';
+const PROXY_URL = window.location.hostname === 'localhost' ? '' : '/proxy';
 
 const instance = axios.create({
   baseURL: `${PROXY_URL}`,
