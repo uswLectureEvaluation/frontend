9058dccc91b4a257f75c793137a3d320b9d1afa8
denebe <twhjdfo01@daum.net>
2022-07-21 13:52:41 +0900

    [feat]이용제한내역

--- src/pages/BanReason/index.jsx
+++ src/pages/BanReason/index.jsx
@@ -1,67 +1,65 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { banListApi, resListApi } from '../../api/Api';
+import * as Styled from './styled';
 
 const BanReason = () => {
+  const [db, setData] = useState([]);
 
-    const [db, setData] = useState([]);
+  const [wow, setWow] = useState([]);
 
-    const [wow, setWow] = useState([]);
+  useEffect(() => {
+    banListApi().then((data) => setData(data));
+    resListApi().then((data) => setWow(data));
+  }, []);
+  console.log(db, wow);
 
-    useEffect(() => {
-        banListApi().then((data) => setData(data));
-        resListApi().then((data) => setWow(data));
-      }, []);
-      console.log(db, wow)
+  return (
+    <Styled.AppContainer>
+      <Styled.AppTitle>블랙리스트 내역조회</Styled.AppTitle>
+
+      {db &&
+        db.map((i) => {
+          return (
+            <Reason
+              key={Math.random()}
+              id={i.id}
+              title={i.blackListReason}
+              judge={i.judgement}
+              cdate={i.createdAt}
+              edate={i.expiredAt}
+            />
+          );
+        })}
       
-    return (
-        <div>
-            <h1>
-                블랙
-            </h1>
-            {
-                db && db.map((i) => (
-                    <div key={Math.random()}>
-                        <div>
-                            {i.blackListReason}
-                        </div>
-                        <h1>
-                        {i.judgement}
-                    </h1>
-                    <h1>
-                    {i.createdAt}
-                </h1>
-                <h1>
-                    {i.expiredAt}
-                </h1>
-                </div>
-                ))
-            }
-            <div>
+        <Styled.AppTitle>이용제한 내역조회</Styled.AppTitle>
 
-            정지
+        {wow &&
+        wow.map((i) => {
+            return (
+            <Reason
+            key={Math.random()}
+            id={i.id}
+            title={i.restrictedReason}
+            judge={i.judgement}
+            cdate={i.createdAt}
+            edate={i.restrictingDate}
+          />
+            );
+         
+    })}
+    </Styled.AppContainer>
+  );
+};
 
-{
-                wow && wow.map((i) => (
-                    <div key={Math.random()}>
-                        <div>
-                            {i.restrictedReason}
-                        </div>
-                        <h1>
-                        {i.judgement}
-                    </h1>
-                    <h1>
-                    {i.createdAt}
-                </h1>
-                <h1>
-                    {i.restrictingDate}
-                </h1>
-                </div>
-                ))
-            }
-            </div>
+export const Reason = (props) => {
+  return (
+    <Styled.NoticeWrap>
+      <Styled.Title>정지사유: {props.title}</Styled.Title>
+      <Styled.Option>제한일시: {props.cdate}</Styled.Option>
+      <Styled.Option>제한기간: {props.edate}</Styled.Option>
+      <Styled.Option>조치사항: {props.judge}</Styled.Option>
+    </Styled.NoticeWrap>
+  );
+};
 
-        </div>
-    )
-}
-
-export default BanReason
+export default BanReason;
