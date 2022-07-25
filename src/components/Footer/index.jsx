5b940d2bce8059366cdd9a9a1079e29560542012
tooooo1 <wer4272@nate.com>
2022-07-26 01:29:12 +0900

    feat: Footer 링크 연동
    이제 Footer의 이용약관과 개인정보처리방침을 누르면 이동됩니다. (연동)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,14 @@ import * as Styled from './styled';
 const Footer = () => {
   return (
     <Styled.Foot>
-      <Styled.Use>이용약관</Styled.Use>
-      <Styled.Privacy>개인정보처리방침</Styled.Privacy>
+      <Styled.Use onClick={() => window.open('https://sites.google.com/view/suwiki-policy-terms/')}>
+        이용약관
+      </Styled.Use>
+      <Styled.Privacy
+        onClick={() => window.open('https://sites.google.com/view/suwiki-policy-privacy')}
+      >
+        개인정보처리방침
+      </Styled.Privacy>
     </Styled.Foot>
   );
 };
