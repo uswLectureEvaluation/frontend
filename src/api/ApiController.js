ea035657d80e77debdb44b80904828217c993020
tooooo1 <wer4272@nate.com>
2022-07-27 16:34:43 +0900

    revert: api 롤백

--- src/api/ApiController.js
+++ src/api/ApiController.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { logoutApi } from './Api';
 
 const PROXY_URL = window.location.hostname === 'localhost' ? '' : '/proxy';
 
@@ -16,8 +15,7 @@ instance.interceptors.request.use(
         config.url.includes('suwiki/version') ||
         config.url.includes('suwiki/majorType') ||
         config.url.includes('lecture/search/?searchValue') ||
-        config.url.includes('notice') ||
-        config.url.includes('client-logout')
+        config.url.includes('notice')
       )
     ) {
       const { data } = await axios({
@@ -33,69 +31,62 @@ instance.interceptors.request.use(
       }
     }
     config.headers['Content-Type'] = 'application/json';
+    if (
+      (config.url.includes('evaluate-posts/?lectureId') ||
+        config.url.includes('exam-posts/?lectureId')) &&
+      config.method === 'post'
+    ) {
+      alert('작성 완료');
+      window.location.reload();
+    } else if (
+      (config.url.includes('evaluate-posts/?evaluateIdx') ||
+        config.url.includes('exam-posts/?examIdx')) &&
+      config.method === 'put'
+    ) {
+      alert('수정 완료');
+      window.location.reload();
+    } else if (
+      (config.url.includes('evaluate-posts/?evaluateIdx') ||
+        config.url.includes('exam-posts/?examIdx')) &&
+      config.method === 'delete'
+    ) {
+      alert('삭제 완료');
+      window.location.reload();
+    } else if (config.url.includes('exam-posts/purchase/?lectureId') && config.method === 'post') {
+      alert('구매 완료');
+      window.location.reload();
+    } else if (
+      config.url.includes('user/report/evaluate') ||
+      config.url.includes('user/report/exam')
+    ) {
+      alert('신고 완료');
+      window.location.reload();
+    } else if (config.url.includes('user/reset-pw')) {
+      alert('변경 완료');
+      window.location.reload();
+    } else if (config.url.includes('user/quit')) {
+      alert('탈퇴 완료');
+      window.location.reload();
+    }
 
     return config;
   },
   function (error) {
-    alert('해당 요청이 정상적으로 이루어지지 않았어요.\n 다시 시도해주세요.');
+    //request 에러
     return Promise.reject(error);
   }
 );
 
 instance.interceptors.response.use(
   function (response) {
-    if (
-      response.config.url.includes('evaluate-posts/?lectureId') ||
-      response.config.url.includes('exam-posts/?lectureId')
-    ) {
-      alert('작성 완료');
-      window.location.reload();
-    }
-    if (
-      response.config.url.includes('evaluate-posts/?evaluateIdx') ||
-      response.config.url.includes('exam-posts/?examIdx')
-    ) {
-      alert('수정 완료');
-      window.location.reload();
-    }
-    if (
-      response.config.url.includes('evaluate-posts/?evaluateIdx') ||
-      response.config.url.includes('exam-posts/?examIdx')
-    ) {
-      alert('삭제 완료');
-      window.location.reload();
-    }
-    if (response.config.url.includes('exam-posts/purchase/?lectureId')) {
-      alert('구매 완료');
-      window.location.reload();
-    }
-    if (
-      response.config.url.includes('user/report/evaluate') ||
-      response.config.url.includes('user/report/exam')
-    ) {
-      alert('신고 완료');
-      window.location.reload();
-    }
-    if (response.config.url.includes('user/reset-pw')) {
-      alert('변경 완료');
-      window.location.reload();
-    }
-    if (response.config.url.includes('user/quit')) {
-      alert('탈퇴 완료');
-      window.location.reload();
-    }
     return response.data;
   },
   async (error) => {
     const originalRequest = error.config;
     if (error.response.status === 403) {
-      logoutApi().then((data) => {
-        if (data.Success) {
-          localStorage.removeItem('login');
-          alert('로그인 시간이 만료되었습니다\n다시 로그인 해주세요');
-          window.location.href = '/';
-        }
-      });
+      localStorage.removeItem('login');
+      alert('로그인 시간이 만료되었습니다\n다시 로그인 해주세요');
+      window.location.href = '/';
     }
     if (error.response.status === 401) {
       const { data } = await axios({
