579ab3c64a15c0acc23629d340ecd5833796c84f
truewayy <dldudwls888@gmail.com>
2022-08-03 14:04:49 +0900

    delete: 필요없는 컴포넌트 삭제(searchlist)

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,5 @@ export { default as Footer } from './Footer';
 export { default as Input } from './Input';
 export { default as MainList } from './MainList';
 export { default as Nav } from './Nav';
-export { default as SearchList } from './SearchList';
 export { default as TestInfo } from './TestInfo';
 export { default as Wrapper } from './Wrapper';
