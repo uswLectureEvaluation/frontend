e22ae1046eccaef98d523b49670107f846931b22
denebe <twhjdfo01@daum.net>
2022-08-11 13:26:58 +0900

    [fix]회원가입 비번

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -48,7 +48,7 @@ const SignUp = () => {
   // 이메일
   const onChangeEmail = useCallback((e) => {
     const emailRegex =
-      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    /^(?=.[a-zA-Z])(?=.[!@#$%^+=-])(?=.[0-9].){8,25}$/;
     const emailCurrent = e.target.value;
 
     setEmail(emailCurrent);
