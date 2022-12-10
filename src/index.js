dc19884fc4864d33f51f2dd3854a62c1d50e0644
tooooo1 <wer4272@nate.com>
2022-12-10 18:09:06 +0900

    create: styles folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { initialize } from 'react-ga';
-import './index.css';
+import './styles/font.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { RecoilRoot } from 'recoil';
