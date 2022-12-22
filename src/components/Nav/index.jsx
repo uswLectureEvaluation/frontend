422711318bc5b16498494fce10b1dca3fe01009e
tooooo1 <wer4272@nate.com>
2022-12-22 20:46:36 +0900

    refactor: mobile button

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -7,10 +7,7 @@ import * as Styled from './styled';
 const Nav = () => {
   const navigate = useNavigate();
   const auth = Auth();
-  const [button, setButton] = useState(true);
   const [click, setClick] = useState(false);
-
-  const showButton = () => (window.innerWidth <= 960 ? setButton(false) : setButton(true));
   const handleClick = () => setClick(!click);
 
   const logoutClick = () => {
@@ -29,8 +26,6 @@ const Nav = () => {
     });
   };
 
-  window.addEventListener('resize', showButton);
-
   return (
     <Styled.Navbar>
       <picture>
@@ -44,39 +39,37 @@ const Nav = () => {
           onClick={() => navigate('/')}
         />
       </picture>
-      {!button && (
-        <Styled.MobileIcon onClick={handleClick}>
-          {click ? (
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 16 16"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
-              ></path>
-            </svg>
-          ) : (
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 512 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
-            </svg>
-          )}
-        </Styled.MobileIcon>
-      )}
+      <Styled.MobileIcon onClick={handleClick}>
+        {click ? (
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
+            ></path>
+          </svg>
+        ) : (
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 512 512"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
+          </svg>
+        )}
+      </Styled.MobileIcon>
 
       <Styled.NavMenu onClick={handleClick} click={click}>
         <Styled.NavLinks onClick={() => navigate('notice')}>공지사항</Styled.NavLinks>
