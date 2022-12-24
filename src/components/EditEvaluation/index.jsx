e24fc50b1135762cb5ac39740ae8b9070694f6db
truewayy <dldudwls888@gmail.com>
2022-12-24 15:39:52 +0900

    style: 중복 제거 작업(1)

--- src/components/EditEvaluation/index.jsx
+++ src/components/EditEvaluation/index.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
-import * as Styled from './styled';
 import { SemesterSelect, StyledOption, Soption } from '../../pages/Main/styled';
 import { useMutation } from 'react-query';
 import { queryClient } from '../..';
 import User from '../../api/User';
 import useSlider from '../RangeInput';
+import {
+  Content,
+  ContentTitle,
+  ContentWrapper,
+  EditButton,
+  FormCheckLeft,
+  FormCheckText,
+  MobileContent,
+  Score,
+  TextField,
+  Title,
+  TitleButton,
+  TitleWrapper,
+  Wrapper,
+} from '../WriteEvaluation/styled';
 
 const EditEvaluation = ({ setModalIsOpen, row }) => {
   const user = User();
@@ -69,21 +83,21 @@ const EditEvaluation = ({ setModalIsOpen, row }) => {
   const optionsValue = options.concat(row.semesterList.split(', '));
 
   return (
-    <Styled.Wrapper>
-      <Styled.TitleWrapper>
-        <Styled.Title>{row.lectureName}</Styled.Title>
-        <Styled.TitleButton
+    <Wrapper>
+      <TitleWrapper>
+        <Title>{row.lectureName}</Title>
+        <TitleButton
           onClick={() => {
             setModalIsOpen(false);
           }}
         >
           X
-        </Styled.TitleButton>
-      </Styled.TitleWrapper>
+        </TitleButton>
+      </TitleWrapper>
 
-      <Styled.ContentWrapper>
-        <Styled.Content id="group">
-          <Styled.ContentTitle>수강학기</Styled.ContentTitle>
+      <ContentWrapper>
+        <Content id="group">
+          <ContentTitle>수강학기</ContentTitle>
           <SemesterSelect
             id="semester"
             defaultValue={`${row.selectedSemester}`}
@@ -97,9 +111,9 @@ const EditEvaluation = ({ setModalIsOpen, row }) => {
               </StyledOption>
             ))}
           </SemesterSelect>
-        </Styled.Content>
-        <Styled.MobileContent id="semester">
-          <Styled.ContentTitle id="mobile">수강학기</Styled.ContentTitle>
+        </Content>
+        <MobileContent id="semester">
+          <ContentTitle id="mobile">수강학기</ContentTitle>
           <SemesterSelect
             id="semester"
             defaultValue={`${row.selectedSemester}`}
@@ -113,128 +127,108 @@ const EditEvaluation = ({ setModalIsOpen, row }) => {
               </StyledOption>
             ))}
           </SemesterSelect>
-        </Styled.MobileContent>
+        </MobileContent>
 
-        <Styled.Content>
-          <Styled.ContentTitle>꿀강지수</Styled.ContentTitle>
-          <HoneySlider /> <Styled.Score>{honey}</Styled.Score>
-        </Styled.Content>
-        <Styled.MobileContent>
+        <Content>
+          <ContentTitle>꿀강지수</ContentTitle>
+          <HoneySlider /> <Score>{honey}</Score>
+        </Content>
+        <MobileContent>
           <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '5px' }}>
-            <Styled.ContentTitle id="mobile">꿀강지수</Styled.ContentTitle>
-            <Styled.Score id="mobile">{honey}</Styled.Score>
+            <ContentTitle id="mobile">꿀강지수</ContentTitle>
+            <Score id="mobile">{honey}</Score>
           </div>
           <HoneySlider />
-        </Styled.MobileContent>
+        </MobileContent>
 
-        <Styled.Content>
-          <Styled.ContentTitle>배움지수</Styled.ContentTitle>
-          <LearingSlider /> <Styled.Score>{learning}</Styled.Score>
-        </Styled.Content>
-        <Styled.MobileContent>
+        <Content>
+          <ContentTitle>배움지수</ContentTitle>
+          <LearingSlider /> <Score>{learning}</Score>
+        </Content>
+        <MobileContent>
           <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '5px' }}>
-            <Styled.ContentTitle id="mobile">배움지수</Styled.ContentTitle>
-            <Styled.Score id="mobile">{learning}</Styled.Score>
+            <ContentTitle id="mobile">배움지수</ContentTitle>
+            <Score id="mobile">{learning}</Score>
           </div>
           <LearingSlider />
-        </Styled.MobileContent>
+        </MobileContent>
 
-        <Styled.Content id="group">
-          <Styled.ContentTitle>만족도</Styled.ContentTitle>
-          <SatisfactionSlider /> <Styled.Score>{satisfaction}</Styled.Score>
-        </Styled.Content>
-        <Styled.MobileContent>
+        <Content id="group">
+          <ContentTitle>만족도</ContentTitle>
+          <SatisfactionSlider /> <Score>{satisfaction}</Score>
+        </Content>
+        <MobileContent>
           <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '5px' }}>
-            <Styled.ContentTitle id="mobile">만족도</Styled.ContentTitle>
-            <Styled.Score id="mobile">{satisfaction}</Styled.Score>
+            <ContentTitle id="mobile">만족도</ContentTitle>
+            <Score id="mobile">{satisfaction}</Score>
           </div>
           <SatisfactionSlider />
-        </Styled.MobileContent>
+        </MobileContent>
 
-        <Styled.Content id="content" onChange={teamChange}>
-          <Styled.ContentTitle>조모임</Styled.ContentTitle>
+        <Content id="content" onChange={teamChange}>
+          <ContentTitle>조모임</ContentTitle>
           <label>
-            <Styled.FormCheckLeft name="team" id="easy" value="0" defaultChecked={team === '0'} />
-            <Styled.FormCheckText>없음</Styled.FormCheckText>
+            <FormCheckLeft name="team" id="easy" value="0" defaultChecked={team === '0'} />
+            <FormCheckText>없음</FormCheckText>
           </label>
           <label>
-            <Styled.FormCheckLeft
-              name="team"
-              id="difficult"
-              value="1"
-              defaultChecked={team === '1'}
-            />
-            <Styled.FormCheckText>있음</Styled.FormCheckText>
+            <FormCheckLeft name="team" id="difficult" value="1" defaultChecked={team === '1'} />
+            <FormCheckText>있음</FormCheckText>
           </label>
-        </Styled.Content>
+        </Content>
 
-        <Styled.Content id="content" onChange={homeworkChange}>
-          <Styled.ContentTitle>과제</Styled.ContentTitle>
+        <Content id="content" onChange={homeworkChange}>
+          <ContentTitle>과제</ContentTitle>
           <label>
-            <Styled.FormCheckLeft
-              name="homework"
-              id="easy"
-              value="0"
-              defaultChecked={homework === '0'}
-            />
-            <Styled.FormCheckText>없음</Styled.FormCheckText>
+            <FormCheckLeft name="homework" id="easy" value="0" defaultChecked={homework === '0'} />
+            <FormCheckText>없음</FormCheckText>
           </label>
           <label>
-            <Styled.FormCheckLeft
+            <FormCheckLeft
               name="homework"
               id="normal"
               value="1"
               defaultChecked={homework === '1'}
             />
-            <Styled.FormCheckText>보통</Styled.FormCheckText>
+            <FormCheckText>보통</FormCheckText>
           </label>
           <label>
-            <Styled.FormCheckLeft
+            <FormCheckLeft
               name="homework"
               id="difficult"
               value="2"
               defaultChecked={homework === '2'}
             />
-            <Styled.FormCheckText>많음</Styled.FormCheckText>
+            <FormCheckText>많음</FormCheckText>
           </label>
-        </Styled.Content>
+        </Content>
 
-        <Styled.Content id="content" onChange={difficultyChange}>
-          <Styled.ContentTitle>학점</Styled.ContentTitle>
+        <Content id="content" onChange={difficultyChange}>
+          <ContentTitle>학점</ContentTitle>
           <label>
-            <Styled.FormCheckLeft
-              name="score"
-              id="easy"
-              value="0"
-              defaultChecked={difficulty === '0'}
-            />
-            <Styled.FormCheckText>너그러움</Styled.FormCheckText>
+            <FormCheckLeft name="score" id="easy" value="0" defaultChecked={difficulty === '0'} />
+            <FormCheckText>너그러움</FormCheckText>
           </label>
           <label>
-            <Styled.FormCheckLeft
-              name="score"
-              id="normal"
-              value="1"
-              defaultChecked={difficulty === '1'}
-            />
-            <Styled.FormCheckText>보통</Styled.FormCheckText>
+            <FormCheckLeft name="score" id="normal" value="1" defaultChecked={difficulty === '1'} />
+            <FormCheckText>보통</FormCheckText>
           </label>
           <label>
-            <Styled.FormCheckLeft
+            <FormCheckLeft
               name="score"
               id="difficult"
               value="2"
               defaultChecked={difficulty === '2'}
             />
-            <Styled.FormCheckText>까다로움</Styled.FormCheckText>
+            <FormCheckText>까다로움</FormCheckText>
           </label>
-        </Styled.Content>
-      </Styled.ContentWrapper>
-      <Styled.TextField defaultValue={row.content} onChange={onChangeContent} rows="15" />
-      <Styled.Wrapper id="button">
-        <Styled.EditButton onClick={onEvaluate}>수정하기</Styled.EditButton>
-      </Styled.Wrapper>
-    </Styled.Wrapper>
+        </Content>
+      </ContentWrapper>
+      <TextField defaultValue={row.content} onChange={onChangeContent} rows="15" />
+      <Wrapper id="button">
+        <EditButton onClick={onEvaluate}>수정하기</EditButton>
+      </Wrapper>
+    </Wrapper>
   );
 };
 
