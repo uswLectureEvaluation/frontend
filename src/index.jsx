62699ad259e86ce21634a430ecf48f302ba62349
tooooo1 <wer4272@nate.com>
2023-01-05 01:10:07 +0900

    feat: react-snap

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { hydrate, render } from 'react-dom';
 import { initialize } from 'react-ga';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { RecoilRoot } from 'recoil';
+import App from './App';
 initialize('UA-74092126-2');
 
 export const queryClient = new QueryClient({
@@ -22,14 +22,28 @@ const PROXY_URL = window.location.hostname === 'localhost' ? '' : '/proxy';
 axios.defaults.baseURL = PROXY_URL;
 axios.defaults.withCredentials = true;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <App />
-        <ReactQueryDevtools />
-      </RecoilRoot>
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (document.getElementById('root').hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <App />
+          <ReactQueryDevtools />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <App />
+          <ReactQueryDevtools />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
