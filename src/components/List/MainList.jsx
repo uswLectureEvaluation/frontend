c9d5dd84bfad5b2fde08f86b5e969380b21be976
truewayy <dldudwls888@gmail.com>
2023-01-14 18:31:00 +0900

    refactor: react-query 모듈화 진행중(검색 페이지)

--- src/components/List/MainList.jsx
+++ src/components/List/MainList.jsx
@@ -2,7 +2,8 @@ import { LectureContainer } from 'components';
 import { fakeLectureList } from 'components/placeholderData';
 import useLectureQuery from 'hooks/useLectureQuery';
 const MainList = () => {
-  const { getMain, mainLoading } = useLectureQuery();
+  const { Main } = useLectureQuery();
+  const { getMain, mainLoading } = Main();
   if (mainLoading) return <LectureContainer data={fakeLectureList} />;
 
   return <LectureContainer data={getMain?.data} />;
--- src/hooks/useLectureQuery.js
+++ src/hooks/useLectureQuery.js
@@ -1,22 +1,55 @@
 import { Lecture } from 'api';
-import { useQuery } from 'react-query';
+import { useEffect } from 'react';
+import { useInfiniteQuery, useQuery } from 'react-query';
 import { useSearchParams } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 
 const useLectureQuery = () => {
   const [searchParams] = useSearchParams();
   const lecture = Lecture();
+  const searchValue = searchParams.get('q') || '';
   const option = searchParams.get('option') || 'modifiedDate';
   const majorType = searchParams.get('majorType') || '전체';
   const major = majorType === '전체' ? '' : majorType;
+  const value = searchValue === 'all' ? '' : searchValue;
 
   // 메인 쿼리(key: 정렬,전공)
-  const { data: getMain, isLoading: mainLoading } = useQuery(
-    ['main', option, major],
-    () => lecture.main(option, 1, major),
-    { keepPreviousData: true }
-  );
+  const Main = () => {
+    const { data: getMain, isLoading: mainLoading } = useQuery(
+      ['main', option, major],
+      () => lecture.main(option, 1, major),
+      { keepPreviousData: true }
+    );
+    return { getMain, mainLoading };
+  };
 
-  return { getMain, mainLoading };
+  // 검색 쿼리(key: 검색어,정렬,전공)
+  const Search = () => {
+    const { ref, inView } = useInView();
+    const {
+      data: getSearch,
+      isLoading: searchLoading,
+      fetchNextPage: getNextSearch,
+      isFetchingNextPage: nextLoading,
+    } = useInfiniteQuery(
+      ['search', value, option, major],
+      ({ pageParam = 1 }) => lecture.search(value, pageParam, option, major),
+      {
+        getNextPageParam: (lastPage) => {
+          if (!lastPage.isLast) return lastPage.nextPage;
+          return undefined;
+        },
+        keepPreviousData: true,
+      }
+    );
+    useEffect(() => {
+      if (inView) {
+        getNextSearch();
+      }
+    }, [inView, getNextSearch]);
+    return { getSearch, searchLoading, nextLoading, value, ref };
+  };
+  return { Main, Search };
 };
 
 export default useLectureQuery;
