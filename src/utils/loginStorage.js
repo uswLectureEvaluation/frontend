fcdd745b6ba3a4440a42500c3cc13834808a5797
tooooo1 <wer4272@nate.com>
2023-01-14 18:47:37 +0900

    refactor: 모듈화

--- src/utils/loginStorage.js
+++ src/utils/loginStorage.js
@@ -1,5 +1,5 @@
 export const isLoginStorage = () => {
-  const state = localStorage.getItem('login') || sessionStorage.getItem('login');
+  const state = localStorage.getItem('login') === null && sessionStorage.getItem('login') === null;
   if (state === null) return false;
   if (state === undefined) return false;
   return state ? true : false;
