3164de402ebc9b972491932559fd1f720bd38ab0
tooooo1 <wer4272@nate.com>
2023-05-07 18:15:21 +0900

    refactor: majorselect component typescript

--- src/components/Major/MajorSelect.tsx
+++ src/components/Major/MajorSelect.tsx
@@ -9,9 +9,17 @@ const MajorSelect = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const majorType = searchParams.get('majorType') || '전체';
 
+  const handleOptionBoxClick = () => {
+    setModalIsOpen(true);
+  };
+
+  const handleModalRequestClose = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <>
-      <OptionBox id="major" onClick={() => setModalIsOpen(true)}>
+      <OptionBox id="major" onClick={handleOptionBoxClick}>
         <SelectedOption id="major">{majorType}</SelectedOption>
         <SelectedOption_M>{subStr(majorType, 3)}</SelectedOption_M>
         <Arrows
@@ -20,7 +28,7 @@ const MajorSelect = () => {
           src={`/images/icon_${modalIsOpen ? 'up' : 'down'}_arrow_solid_24.svg`}
         />
       </OptionBox>
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+      <Modal isOpen={modalIsOpen} onRequestClose={handleModalRequestClose}>
         <MajorSearch setModalIsOpen={setModalIsOpen} />
       </Modal>
     </>
