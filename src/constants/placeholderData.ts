0769ee4ad56150f51de5e4b04bc164756c0f839c
tooooo1 <wer4272@nate.com>
2023-05-08 18:20:44 +0900

    feat: createFakeData utils

--- src/constants/placeholderData.ts
+++ src/constants/placeholderData.ts
@@ -1,3 +1,5 @@
+import createFakeData from 'utils/fakeData';
+
 export const fakeLectureInfo = {
   id: 'fake',
   lectureDifficultyAvg: 1,
@@ -14,44 +16,38 @@ export const fakeLectureInfo = {
   semesterList: '0000-0',
 };
 
-export const fakeLectureList = new Array(10);
-for (let i = 0; i < fakeLectureList.length; i++) {
-  fakeLectureList[i] = {
-    id: 'fake' + i,
-    lectureHoneyAvg: 0,
-    lectureLearningAvg: 0,
-    lectureName: '로딩중입니다',
-    lectureSatisfactionAvg: 0,
-    lectureTotalAvg: 0,
-    lectureType: '로딩',
-    majorType: '로딩',
-    professor: '로딩중',
-    semesterList: '0000-0',
-    content:
-      'suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder',
-  };
-}
+export const fakeLectureList = createFakeData(10, (i) => ({
+  id: 'fake' + i,
+  lectureHoneyAvg: 0,
+  lectureLearningAvg: 0,
+  lectureName: '로딩중입니다',
+  lectureSatisfactionAvg: 0,
+  lectureTotalAvg: 0,
+  lectureType: '로딩',
+  majorType: '로딩',
+  professor: '로딩중',
+  semesterList: '0000-0',
+  content:
+    'suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder',
+}));
 
-export const fakeEvaluationList = new Array(3);
-for (let i = 0; i < fakeEvaluationList.length; i++) {
-  fakeEvaluationList[i] = {
-    id: 'fake' + i,
-    selectedSemester: '0000-0',
-    title: 'suwiki',
-    learning: 0.0,
-    honey: 0.0,
-    satisfaction: 0.0,
-    totalAvg: 0.0,
-    homework: 0.0,
-    difficulty: 0.0,
-    team: 0.0,
-    examDifficulty: '보통',
-    examInfo: '보통, 보통, 보통',
-    examType: '중간고사',
-    content:
-      'suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder',
-  };
-}
+export const fakeEvaluationList = createFakeData(3, (i) => ({
+  id: 'fake' + i,
+  selectedSemester: '0000-0',
+  title: 'suwiki',
+  learning: 0.0,
+  honey: 0.0,
+  satisfaction: 0.0,
+  totalAvg: 0.0,
+  homework: 0.0,
+  difficulty: 0.0,
+  team: 0.0,
+  examDifficulty: '보통',
+  examInfo: '보통, 보통, 보통',
+  examType: '중간고사',
+  content:
+    'suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder suwiki-lecture-content-placeholder',
+}));
 
 export const fakeUserInfo = {
   email: '-',
@@ -72,7 +68,7 @@ export const sortOptions = [
   { sub: '종합', name: 'BEST 강의', option: 'lectureTotalAvg', icon: 'best' },
 ];
 
-export const semesters = (semesterList) => ['선택', ...semesterList.split(', ')];
+export const semesters = (semesterList: string) => ['선택', ...semesterList.split(', ')];
 export const examTypes = ['선택', '중간고사', '기말고사', '쪽지', '기타'];
 
 export const EvaluationSliderOptions = [
