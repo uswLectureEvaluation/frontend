b8bd8b8e9df42719764ac12809af11223ed6cfda
tooooo1 <wer4272@nate.com>
2023-05-09 16:49:07 +0900

    refactor: version check typescript

--- src/app/versionCheck.ts
+++ src/app/versionCheck.ts
@@ -8,26 +8,27 @@ export const versionCheck = () => {
   if (!getStorage('version') || !getStorage('majorType')) {
     version().then((res) => {
       // console.log('버전없어서 세팅');
-      setStorage('version', floatFix(res.version, 1));
+      if (res) setStorage('version', floatFix(res.version, 1));
     });
     type().then((res) => {
       // console.log('전공없어서 세팅');
-      setStorage('majorType', ['전체', res.data]);
+      if (res) setStorage('majorType', ['전체', res.data]);
     });
-    window.sessionStorage.setItem('version-check', true);
+    window.sessionStorage.setItem('version-check', 'true');
     // console.log('버전 체크 완료');
   } else if (!window.sessionStorage.getItem('version-check')) {
     version().then((res) => {
+      if (!res) return;
       if (getStorage('version') !== floatFix(res.version, 1)) {
         // console.log('버전 다름');
         setStorage('version', floatFix(res.version, 1));
         // console.log('버전 최신화');
         type().then((res) => {
-          setStorage('majorType', ['전체', res.data]);
+          if (res) setStorage('majorType', ['전체', res.data]);
           // console.log('전공 최신화');
         });
       }
-      window.sessionStorage.setItem('version-check', true);
+      window.sessionStorage.setItem('version-check', 'true');
       // console.log('버전 체크 완료');
     });
   }
