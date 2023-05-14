98547a317fdf1f5c27c77d1e4967a7a6ec31a54d
tooooo1 <wer4272@nate.com>
2023-05-14 18:34:22 +0900

    feat: lecture type

---
+++ src/types/lecture.ts
@@ -0,0 +1,12 @@
+export interface MainLecture {
+  id: number;
+  semesterList: string;
+  professor: string;
+  majorType: string;
+  lectureType: string;
+  lectureName: string;
+  lectureTotalAvg: number;
+  lectureSatisfactionAvg: number;
+  lectureHoneyAvg: number;
+  lectureLearningAvg: number;
+}
