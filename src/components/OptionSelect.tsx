f48351760fd365d98d4c2edcc4a6e65b79f8ad9c
tooooo1 <wer4272@nate.com>
2023-05-17 02:48:27 +0900

    refactor: OptionSelect 구조 개선

--- src/components/OptionSelect.tsx
+++ src/components/OptionSelect.tsx
@@ -1,29 +1,21 @@
+import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Arrows, Option, OptionBox, Options, SelectedOption } from 'styles/common';
 import { SortOption } from 'types/common';
 
 interface OptionSelectProps {
   list: SortOption[];
-  state: boolean;
-  controller: (state: boolean) => void;
   itemTitle: keyof SortOption;
-  icon?: boolean;
   location: string;
 }
 
-const OptionSelect = ({
-  list,
-  state,
-  controller,
-  itemTitle,
-  icon = true,
-  location,
-}: OptionSelectProps) => {
+const OptionSelect = ({ list, itemTitle, location }: OptionSelectProps) => {
+  const [select, onSelect] = useState(false);
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const searchValue = searchParams.get('q');
-  const option = searchParams.get('option') || 'modifiedDate';
-  const majorType = searchParams.get('majorType') || '전체';
+  const searchValue = searchParams.get('q') ?? '';
+  const option = searchParams.get('option') ?? 'modifiedDate';
+  const majorType = searchParams.get('majorType') ?? '전체';
   const selectedOption = list.find((row) => row.option === option) as SortOption;
   const OptionName = selectedOption[itemTitle];
   const isMain = location === 'main';
@@ -37,16 +29,16 @@ const OptionSelect = ({
   return (
     <OptionBox
       id={String(isMain)}
-      select={state}
-      icon={icon ? selectedOption?.icon : undefined}
+      select={select}
+      icon={isMain ? selectedOption?.icon : undefined}
       onClick={(e) => {
         e.stopPropagation;
-        controller(!state);
+        onSelect(!select);
       }}
     >
       <SelectedOption id={String(isMain)}>{OptionName}</SelectedOption>
-      <Arrows src={`/images/icon_${state ? 'up' : 'down'}_arrow_solid_24.svg`} />
-      {state && (
+      <Arrows src={`/images/icon_${select ? 'up' : 'down'}_arrow_solid_24.svg`} />
+      {select && (
         <Options>
           {list.map((option) => {
             return (
