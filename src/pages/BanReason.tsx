a25dd98b7b90a4a089e47e58fa3298ad85f4bf09
tooooo1 <wer4272@nate.com>
2023-05-18 13:28:08 +0900

    refactor: 타입스크립트 마이그레이션 및 서스펜스, 애러바운더리 적용, 보다 명확한 리터럴 적용 (type -> banType 등)

--- src/pages/BanReason.tsx
+++ src/pages/BanReason.tsx
@@ -1,24 +1,25 @@
 import styled from '@emotion/styled';
-import { useQuery } from 'react-query';
 import { User } from 'api';
-import { Spinner, BanFrame } from 'components';
+import { BanFrame } from 'components';
+import { useQuery } from 'react-query';
 import { AppContainer } from 'styles/common';
 
 const BanReason = () => {
   const user = User();
-  const { data: banList, isLoading: banLoad } = useQuery(['myInfo', 'banList'], user.banList);
-  const { data: resList, isLoading: resLoad } = useQuery(['myInfo', 'resList'], user.resList);
-
-  if (banLoad) return <Spinner id="myInfo" />;
-  if (resLoad) return null;
+  const { data: banList } = useQuery(['myInfo', 'banList'], user.banList, {
+    suspense: true,
+  });
+  const { data: resList } = useQuery(['myInfo', 'resList'], user.resList, {
+    suspense: true,
+  });
 
   return (
     <AppContainer>
       <AppTitle>블랙리스트 내역조회</AppTitle>
-      <BanFrame type="ban" list={banList} />
+      <BanFrame banType="ban" list={banList} />
 
       <AppTitle>이용제한 내역조회</AppTitle>
-      <BanFrame type="res" list={resList} />
+      <BanFrame banType="res" list={resList} />
     </AppContainer>
   );
 };
