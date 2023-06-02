c5a96c8f7541a2c3251114a3a242ca30d49aa90e
tooooo1 <wer4272@nate.com>
2023-06-02 19:46:27 +0900

    refactor: writeevaluation-majorsearch-typescript

--- src/hooks/useWriteEvaluation.ts
+++ src/hooks/useWriteEvaluation.ts
@@ -1,8 +1,9 @@
 import { User } from 'api';
 import useSlider from 'components/Etc/RangeInput';
+import type { WriteEvaluationProps } from 'components/Write/WriteEvaluation';
 import { useState } from 'react';
 
-const useWriteEvaluation = ({ setModalIsOpen, row, type }) => {
+const useWriteEvaluation = ({ setModalIsOpen, row, type }: WriteEvaluationProps) => {
   const { updateEvaluation, writeEvaluation } = User();
   const [content, setContent] = useState(row.content);
   const [selectedSemester, setSelectedSemester] = useState(row.selectedSemester); //학기
@@ -30,12 +31,10 @@ const useWriteEvaluation = ({ setModalIsOpen, row, type }) => {
     content,
   };
 
-  const onChangeContent = (e) => {
-    setContent(e.target.value);
-  };
-  const onChangeLectureOptions = (e) => {
+  const onChangeContent = (e: any) => setContent(e.target.value);
+
+  const onChangeLectureOptions = (e: any) =>
     setLectureOptions({ ...lectureOptions, [e.target.name]: e.target.value });
-  };
 
   const onEvaluate = () => {
     if (selectedSemester === '' || selectedSemester === '선택') return alert('학기를 선택해주세요');
@@ -45,12 +44,14 @@ const useWriteEvaluation = ({ setModalIsOpen, row, type }) => {
       return alert('배움지수는 0.5점부터 선택 가능합니다');
     if (SliderOptions.satisfaction.state < 0.5 || SliderOptions.satisfaction.state === undefined)
       return alert('만족도는 0.5점부터 선택 가능합니다');
-    if (lectureOptions.team === '') return alert('조모임(란)을 선택해주세요');
-    if (lectureOptions.homework === '') return alert('과제(란)을 선택해주세요');
-    if (lectureOptions.difficulty === '') return alert('학점(란)을 선택해주세요');
+    if (lectureOptions.team === undefined) return alert('조모임(란)을 선택해주세요');
+    if (lectureOptions.homework === undefined) return alert('과제(란)을 선택해주세요');
+    if (lectureOptions.difficulty === undefined) return alert('학점(란)을 선택해주세요');
     if (content.length < 30 || content.length > 1000)
       return alert('최소 30자 이상 최대 1000자 이내로 입력해주세요');
-    type === 'update' ? updateEvaluation(row.id, data) : writeEvaluation(row.id, data);
+    type === 'update'
+      ? updateEvaluation(row.id.toString(), data)
+      : writeEvaluation(row.id.toString(), data);
     setModalIsOpen(false);
   };
   return {
